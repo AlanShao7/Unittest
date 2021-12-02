c643432520c65b372f63bee0fb22a0253072ae87
81538 <815385442@qq.com>
2021-12-02 16:47:51 +0800

    测试

--- case_lib/customers/customers_case.py
+++ case_lib/customers/customers_case.py
@@ -4,28 +4,35 @@ import os
 file_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(file_path)
 from function_lib.customers.customer import Customer
+from function_lib.rights_management.management import RightManagement
 
 
 class Customer_case(unittest.TestCase):
 
     def setUp(self) -> None:
-        pass
+        self.customer_operation_permissions = RightManagement().permission('customer')
+        self.customer_transfer_permissions = Customer.customers_permissions()
 
     def test001(self):
         """
         判断权限获取是否正确问题
         :return:
         """
-        permissions = Customer.customers_permissions()
-        print(permissions.json())
-        self.assertEqual(permissions.json()['code'], '0')
+        self.assertEqual(self.customer_transfer_permissions.json()['code'], '0')
 
     def test002(self):
         """
-
+        判断权限和新增问题
+        有权限200  没权限401
         :return:
         """
-        pass
+        res = Customer.add_customer_one('ceshi')
+        if 'create' in self.customer_operation_permissions:
+            # print(res.status_code)
+            self.assertEqual(res.status_code, 200)
+        else:
+            # print(res.status_code)
+            self.assertEqual(res.status_code, 401)
 
 
 if __name__ == '__main__':
--- function_lib/rights_management/management.py
+++ function_lib/rights_management/management.py
@@ -3,9 +3,32 @@ import sys
 
 file_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(file_path)
+from function_lib.public_func import HttpRequest
 
 
-class Right_management:
+class RightManagement:
 
     def __init__(self):
-        pass
+        res = HttpRequest().send_request('/api/pc/permissions', 'get')
+        self.all_permission = res.json()['data']['permissions']
+
+    def permission(self, mode):
+        """
+        查看模块的操作权限
+        :param mode:选择模块：subject
+        name: 权限中文名称，对应一种业务权限。
+        subject: 操作的对象模块名，对应业务模块，是业务名称英文下划线小写字母组成的单词。
+        action： 权限对应的具体操作，一般情况： show=查看， edit=编辑， destroy=删除， create=新增，
+        update=更新，crud=增删改查，merge=合并，enable=开启，disable=禁用，import=导入，export=导出
+        :return:返回的是某一个模块下有的权限  例如customer下有编辑权限 则返回列表中有edit
+        """
+        user_list = []
+        for i in self.all_permission:
+            if i['subject'] == mode:
+                user_list.append(i['action'])
+        return user_list
+
+
+if __name__ == '__main__':
+    a = RightManagement()
+    print(a)
